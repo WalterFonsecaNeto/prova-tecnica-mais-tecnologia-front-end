ff691ce49fcc743ae7a78f66efb80c0d57eb5df4
Walter Fonseca <walterfonseca1606@gmail.com>
2025-03-16 19:47:26 -0300

    Criando filtro de pesquisa para nome e email

--- src/app/components/pessoa/pessoa.component.ts
+++ src/app/components/pessoa/pessoa.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { TopbarComponent } from "../topbar/topbar.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { CardPessoaComponent } from "../card-pessoa/card-pessoa.component";
@@ -6,24 +7,38 @@ import { PessoaApiService } from '../../services/pessoa-api.service';
 
 @Component({
   selector: 'app-pessoa',
-  imports: [TopbarComponent, SidebarComponent, CardPessoaComponent],
+  imports: [
+    FormsModule,
+    TopbarComponent,
+    SidebarComponent,
+    CardPessoaComponent,
+  ],
   templateUrl: './pessoa.component.html',
   styleUrl: './pessoa.component.css'
 })
-export class PessoaComponent implements OnInit  {
-  constructor(
-    private pessoaApi: PessoaApiService
-  ) { }
+export class PessoaComponent implements OnInit {
+  
+  constructor(private pessoaApi: PessoaApiService) {}
+
   pessoas: any[] = [];
+  nomeFiltro: string = "";
+  emailFiltro: string = "";
 
   ngOnInit(): void {
     this.buscarPessoa();
   }
 
+  
+  get pessoasFiltradas() {
+    return this.pessoas.filter(pessoa => 
+      pessoa.nome.toLowerCase().startsWith(this.nomeFiltro.toLowerCase()) &&
+      pessoa.email.toLowerCase().startsWith(this.emailFiltro.toLowerCase())
+    );
+  }
+  
+  
 
-  async buscarPessoa(){
+  async buscarPessoa() {
     this.pessoas = await this.pessoaApi.buscarPessoas();
   }
-
-
 }
